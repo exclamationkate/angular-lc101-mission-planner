a3fa0db51b5fcf91f1cae6b92383abc58bb8a501
Kathryn Steib <kate.steib@gmail.com>
2019-10-26 18:28:13 -0500

    Part 2 complete.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,13 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(member) {
+    if (!this.crew.includes(member) && this.crew.length < 3) {
+      this.crew.push(member);
+    } else if (this.crew.includes(member)){
+      let index = this.crew.indexOf(member);
+      this.crew.splice(index, 1);
+    }
+  }
 
 }
